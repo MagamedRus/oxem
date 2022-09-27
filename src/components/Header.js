9eb350e137659533d1c11c8efa4827086f5606fe
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-27 16:40:23 +0500

    add main header

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyledH1 } from "../styledComponents/Headers";
+
+const Header = ({ title }) => <StyledH1>{title}</StyledH1>;
+
+export default Header;
---
+++ src/styledComponents/Headers.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const StyledH1 = styled.h1`
+  font-family: "Nekst-Black";
+  font-style: normal;
+  font-weight: 900;
+  font-size: 54px;
+  line-height: 90%;
+  color: #111111;
+  margin: 0;
+`;
+
+export const StyledH2 = styled.h2`
+  margin: auto 24px;
+  font-family: "Nekst-Black";
+  font-style: normal;
+  font-weight: 900;
+  font-size: 30px;
+  line-height: 36px;
+  color: #575757;
+`;
+
+export const StyledH3 = styled.h3`
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
+  color: #575757;
+`;

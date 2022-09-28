1251022546ebcedf2e43cce050b474448d92f18d
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-28 14:54:18 +0500

    add TitledPrice

---
+++ src/components/TitledPrice.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import { numberWithSpaces } from "../common/composeNumber";
+import { GilroyH3 } from "../styledComponents/Headers";
+import { BoldP30 } from "../styledComponents/Paragraphs";
+
+const TitledPrice = ({ price, title }) => (
+  <Container>
+    <Title>{title}</Title>
+    <BoldP30>{numberWithSpaces(price)} ₽</BoldP30>
+  </Container>
+);
+
+export default TitledPrice;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 49px;
+  width: max-content;
+`;
+
+const Title = styled(GilroyH3)`
+  line-height: 24px;
+`;

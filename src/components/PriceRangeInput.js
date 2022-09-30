372ded23d71f3503ee46f550236ed95f9787ade4
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-30 16:53:22 +0500

    add debounced typing by user to PriceRangeInput

--- src/components/PriceRangeInput.js
+++ src/components/PriceRangeInput.js
@@ -1,33 +1,68 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { GilroyH3 } from "../styledComponents/Headers";
 import { BoldP30 } from "../styledComponents/Paragraphs";
 import ValuedRangeInput from "./ValuedRangeInput";
-import { setProcentPrice, setValuePrice } from "../store/action-creators/input";
+import {
+  setIsTyping,
+  setProcentPrice,
+  setValuePrice,
+} from "../store/action-creators/input";
 import { numberWithSpaces } from "../common/composeNumber";
 import { TransparentTextInput } from "../styledComponents/Inputs";
 
 const PriceRangeInput = () => {
-  const dispatch = useDispatch();
   const price = useSelector((state) => state.input.price);
-  const setProcentValue = (procentValue) => dispatch(setProcentPrice(procentValue));
+  const [value, setValue] = useState(price.value);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const dispatch = useDispatch();
+  const textInputRef = useRef();
 
   const onTextInput = (ev) => {
-    let value = ev.target.value;
-    value = parseInt(value.replaceAll(" ", "").replaceAll("₽", "")) || 0;
-    dispatch(setValuePrice(value, price.value));
+    let currValue = ev.target.value;
+    currValue = parseInt(currValue.replaceAll(" ", "")) || 0;
+
+    setValue(currValue);
+    !textInputRef.typingTimer && setTypingState(true);
+    clearTimeout(textInputRef.typingTimer);
+    textInputRef.typingTimer = setTimeout(onEndType.bind(this, currValue), 500);
   };
 
+  // Change is focus state for make input white
+  const toggleFocus = () => setIsFocus((prevState) => !prevState);
+
+  const onEndType = (currValue) => {
+    dispatch(setValuePrice(currValue));
+    setTypingState(false);
+  };
+
+  const setProcentValue = (procentValue) =>
+    dispatch(setProcentPrice(procentValue));
+
+  // Change is typing state in reducer
+  const setTypingState = (isTyping) => dispatch(setIsTyping(isTyping));
+
+  // Set calced state from redux to inner state(;
+  useEffect(() => setValue(price.value), [price]);
 
   return (
     <Container>
       <GilroyH3>Стоимость автомобиля</GilroyH3>
-      <ValuedRangeInput value={price.procentValue} setValue={setProcentValue} step={4}>
+      <ValuedRangeInput
+        value={price.procentValue}
+        setValue={setProcentValue}
+        step={4}
+        isFocus={isFocus}
+      >
         <ValueContainer>
           <TransparentTextInput
+            ref={textInputRef}
+            onFocus={toggleFocus}
+            onBlur={toggleFocus}
             onInput={onTextInput}
-            value={numberWithSpaces(price.value)}
+            value={numberWithSpaces(value)}
           />
           <BoldP30>₽</BoldP30>
         </ValueContainer>
